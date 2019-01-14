8cfade81576a41d47548b9d11a8808ad5ade6ef3
eg913 <eg913@pvsd.org>
2019-01-14 11:21:18 -0500

    UGGGHH

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner numOfMoves = new Scanner(System.in);
+        System.out.println("Please input the number of disks to solve for: ");
+        int a = numOfMoves.nextInt();
+        if (a >= 3 && a <= 9) {
+            TowersOfHanoi solution = new TowersOfHanoi(a);
+        }
+        else {
+            System.out.println("Invalid input your number must be between 3 and 9.");
+            System.out.println("Please input the number of disks to solve for: ");
+            int b = numOfMoves.nextInt();
+            TowersOfHanoi solution = new TowersOfHanoi(b);
+        }
+
     }
 }
